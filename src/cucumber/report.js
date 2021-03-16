98d25c93cb2648830b752613994dcd28504e7acf
suhasmy29 <suhasmy29@gmail.com>
2021-03-16 16:15:36 +0530

    reports for latest testcases pushed

--- src/cucumber/report.js
+++ src/cucumber/report.js
@@ -39,7 +39,7 @@ formatter.match({
   "location": "loginToSwagLabs.user_is_on_swaglabs_website_loginpage()"
 });
 formatter.result({
-  "duration": 18282486000,
+  "duration": 12224324600,
   "status": "passed"
 });
 formatter.match({
@@ -56,14 +56,80 @@ formatter.match({
   "location": "loginToSwagLabs.user_enters_username_something_and_password_something(String,String)"
 });
 formatter.result({
-  "duration": 2888023700,
+  "duration": 843902100,
   "status": "passed"
 });
 formatter.match({
   "location": "loginToSwagLabs.home_page_is_displayed()"
 });
 formatter.result({
-  "duration": 2364830800,
+  "duration": 1232811100,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "Footer text validation",
+  "description": "",
+  "id": "swaglabs-website;footer-text-validation",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 9,
+      "name": "@Smoke"
+    }
+  ]
+});
+formatter.step({
+  "line": 11,
+  "name": "User is on Swaglabs website loginpage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "user enters username \u003d \"standard_user\" and Password \u003d \"secret_sauce\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "footer text \u003d \"© 2021 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy\" is displayed in the home Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "loginToSwagLabs.user_is_on_swaglabs_website_loginpage()"
+});
+formatter.result({
+  "duration": 6448344700,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "standard_user",
+      "offset": 24
+    },
+    {
+      "val": "secret_sauce",
+      "offset": 55
+    }
+  ],
+  "location": "loginToSwagLabs.user_enters_username_something_and_password_something(String,String)"
+});
+formatter.result({
+  "duration": 748301000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "© 2021 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy",
+      "offset": 15
+    }
+  ],
+  "location": "loginToSwagLabs.footer_text_something_is_displayed_in_the_home_page(String)"
+});
+formatter.result({
+  "duration": 1074871500,
   "status": "passed"
 });
 formatter.uri("Logout.feature");
@@ -117,7 +183,7 @@ formatter.match({
   "location": "loginToSwagLabs.user_is_on_swaglabs_website_loginpage()"
 });
 formatter.result({
-  "duration": 9354372900,
+  "duration": 6046663800,
   "status": "passed"
 });
 formatter.match({
@@ -134,28 +200,28 @@ formatter.match({
   "location": "loginToSwagLabs.user_enters_username_something_and_password_something(String,String)"
 });
 formatter.result({
-  "duration": 738292000,
+  "duration": 798374600,
   "status": "passed"
 });
 formatter.match({
   "location": "loginToSwagLabs.click_on_Menu()"
 });
 formatter.result({
-  "duration": 166018800,
+  "duration": 216696800,
   "status": "passed"
 });
 formatter.match({
   "location": "loginToSwagLabs.click_on_logout()"
 });
 formatter.result({
-  "duration": 248386900,
+  "duration": 191425300,
   "status": "passed"
 });
 formatter.match({
   "location": "loginToSwagLabs.again_login_page_is_displayed()"
 });
 formatter.result({
-  "duration": 1573595900,
+  "duration": 969942900,
   "status": "passed"
 });
 });
